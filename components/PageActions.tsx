017c24b59582bafe7769ffd91403539d67d8033e
longht0110 <154035485+longht0110@users.noreply.github.com>
2025-04-30 15:10:58 +0700

    khong biet ghi commit

--- components/PageActions.tsx
+++ components/PageActions.tsx
@@ -5,9 +5,6 @@ import { IoHeartOutline } from '@react-icons/all-files/io5/IoHeartOutline'
 
 import styles from './styles.module.css'
 
-/**
- * @see https://developer.twitter.com/en/docs/twitter-for-websites/web-intents/overview
- */
 export const PageActions: React.FC<{ tweet: string }> = ({ tweet }) => {
   return (
     <div className={styles.pageActions}>
--- site.config.ts
+++ site.config.ts
@@ -17,9 +17,9 @@ export default siteConfig({
   description: 'Personal Website by Hoang Long.',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  //twitter: 'nguyenhlong2203',
+  github: 'longht0110',
+  // linkedin: 'fisch2',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
