3410489b07e8c94a377bd1fb19165cbb4a2d9292
longht0110 <154035485+longht0110@users.noreply.github.com>
2025-04-30 11:57:53 +0700

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,19 +2,19 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '1e5b3f13175780e29858cb6f9c859d7f',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'DayLaLong',
+  domain: 'daylalong.vercel.app',
+  author: 'Hoang Long',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Personal Website by Hoang Long.',
 
   // social usernames (optional)
   twitter: 'transitive_bs',
